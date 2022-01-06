05a0446024303386b59850a9767580f22b066fdb
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2022-01-07 00:47:27 +0300

    vue install project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import { createApp } from "vue";
+
+import Home from "./components/Home.vue";
+
+const app = createApp({
+
+})
+
+
+app.component('home', Home);
+
+app.mount('#app');
+
